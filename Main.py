c3c00ba71ae46e6e1320f8ca8825dd33720b69f1
salsabeel-tn <salsabeelj66@gmail.com>
2021-08-22 11:58:25 +0300

    project data files added

---
+++ Main.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+#read in dataset
+apps_with_duplicates = pd.read_csv()
+
+# Drop duplicates from apps_with_duplicates
+apps = ...
+
+# Print the total number of apps
+print('Total number of apps in the dataset = ', ...)
+
+# Have a look at a random sample of 5 rows
+print(...)
